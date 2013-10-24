f599b4f76e39d84345fee07a62fcbbacb64b30ae
Jonathan Morra <jonmorra@gmail.com>
2013-10-23 17:37:00 -0700

    Initial commit with RESTEasy working

---
+++ src/main/java/com/tilofy/PhotoQueueController.java
@@ -0,0 +1,20 @@
+package com.tilofy;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * Did you remember documentation? Test cases?
+ *
+ */
+@Path("test")
+public class PhotoQueueController {
+    @GET
+    public Response showMessage() {
+
+        String result = "Hello World2";
+
+        return Response.status(200).entity(result).build();
+    }
+}
